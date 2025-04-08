62be5cb16df5a0fd9daa5e4e4ee4942b8fbb91d9
augustine-ugwu <augustinehills@yahoo.com>
2025-04-08 09:23:24 +0100

    Quick fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import Admin from "../models/Admin.js"; // Import the Admin model
+import Admin from "../models/Admin.js";
 
 const router = express.Router();
 
